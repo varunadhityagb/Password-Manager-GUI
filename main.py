0506248675fdee0a4a3af4c1a0d59b88cdfb267f
Varun Adhitya G B <varunadhitya.balaji@gmail.com>
2022-04-10 14:39:47 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from database import *
-from mypfuncs import *
-
+from database import *          #####IMPORTING OUR COUSTOM MODULES
+from mypfuncs import *          
+#ProjectbyVarunAdhityaGB
 ########################## CHECKING AND CREATING DATABASE #############################
 mycur.execute('SHOW SCHEMAS;')
 db_ls = []
-
+#ProjectbyVarunAdhityaGB
 for i in mycur:
     db_ls.extend(i)
-    
+#ProjectbyVarunAdhityaGB    
 if 'MYP' in db_ls:
     mycur.execute("USE MYP;")
 elif 'myp' in db_ls:
@@ -15,16 +15,16 @@ elif 'myp' in db_ls:
 else:
     createDB()
     mycur.execute("USE MYP;")
-
+#ProjectbyVarunAdhityaGB
 ########################## CHECKING AND CREATING TABLES ###############################
 mycur.execute('SHOW TABLES;')
 tb_ls = []
-
+#ProjectbyVarunAdhityaGB 
 for i in mycur:
     tb_ls.extend(i)
 if tb_ls == []:
     createTbls()
 else:
     pass
-
+#ProjectbyVarunAdhityaGB
 login_page()
