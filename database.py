7c2331b343c9db5df5afc9133a56a3ec78c6edc0
VarunAdhityaGB <88021294+VarunAdhityaGB@users.noreply.github.com>
2022-04-24 00:56:36 +0530

    Created basic ui

--- database.py
+++ database.py
@@ -59,160 +59,13 @@ def insintodata(website, loginName, loginPass, userId):
     mydb.commit()
 
 def retrieve(n: int):
-    # this fucntion retrives data from the myp_data table
-    # given that the userId is given 
-    mycur.execute("USE myp;")
-    mycur.execute("SET @ROW := 0 ;")
-    mycur.execute(f"""SELECT @ROW := @ROW + 1 , loginName
-    FROM myp_data
-    WHERE userId = {n};""") 
-    data = mycur.fetchall()
-
-    mycur.execute(f"""SELECT loginPass
-    FROM myp_data
-    WHERE userId = {n};""") 
-    data_ls = mycur.fetchall()
-    
-    ls = []
-    for i in data_ls:
-        ls.extend(i)   
-    datadict = {}
-    for j in range(len(ls)):
-        datadict[j+1] = ls[j]
-
-    if datadict != {}:
-        print(tabulate(data, headers=['Sl.No.', 'Username'], tablefmt='psql'))
-        print("Enter the number corresponding to the website to check the password:")   
-        datain = int(input())
-        print()
-        print()
-        print(decrypt(datadict[datain]))
-        pyperclip.copy(decrypt(datadict[datain]))
-        print()
-        print()
-        print("YOUR PASSWORD IS COPIED TO YOUR CLIPBOARD🙂")
-    else:
-        print("THERE ARE NO PASSWORDS SAVED FOR THIS USER.")
-    time.sleep(5)
-    post_login() 
+    pass
 
 def deletedata(n: int):
-    # this fucntion deletes data from the myp_data table
-    # given that the userId is given 
-    mycur.execute("USE myp;")
-    mycur.execute("SET @ROW := 0")
-    mycur.execute(f"""SELECT @ROW := @ROW + 1 , loginName
-    FROM myp_data
-    WHERE userId = {n};""") 
-    data = mycur.fetchall()
-
-    mycur.execute(f"""SELECT passId
-    FROM myp_data
-    WHERE userId = {n};""") 
-    data_ls = mycur.fetchall()
-    
-    ls = []
-    for i in data_ls:
-        ls.extend(i)
-
-    datadict = {}
-    for j in range(len(ls)):
-        datadict[j+1] = ls[j]
-
-    if datadict != {}:
-        print(tabulate(data, headers=['Sl.No.', 'Username'], tablefmt='psql'))
-        print("Enter the number corresponding to the website to delete the record:")   
-        datain = int(input())
-        dataout = datadict[datain]
-        print()
-        action = f"DELETE FROM myp_data WHERE (passId = {dataout});"
-        mycur.execute(action)
-        mydb.commit()
-        print("SUCCESSFULLY DELETED THE SELECTED RECORD")
-    else:
-        print("THERE ARE NO PASSWORDS SAVED FOR THIS USER.")
-    time.sleep(5)
-    post_login()
+    pass
 
 def deleteuser(n: int):
-    # this fucntion deletes user from the myp_users table
-    print("Are you sure you want to delete your account? ")
-    deleinp = input("Y/N ? ")
-    if deleinp.upper() == 'Y':
-        mycur.execute("USE myp;")
-
-        mycur.execute("USE myp;")
-        mycur.execute(f"""SELECT eMail
-        FROM myp_users
-        WHERE userId = {n}""")
-        emaild = mycur.fetchall()
-
-        els = []
-        for i in emaild:
-            els.extend(i)
-
-        mycur.execute(f"""SELECT passId 
-        FROM myp_data 
-        WHERE userId = {n};""")
-        data = mycur.fetchall()
-
-        ls = []
-        for i in data:
-            ls.extend(i)
-        
-        for i in ls:
-            mycur.execute(f"""DELETE FROM myp_data
-            WHERE (passId = {i});""")
-            mydb.commit()
-        
-        mycur.execute(f"""DELETE FROM myp_users 
-        WHERE (userId = {n});""")
-        mydb.commit()
-
-        print("Successfully deleted your details and data from our database")
-        byemail(els[0])     
-        time.sleep(5)
-        login_page()
-
-    elif deleinp.upper() == 'N':
-            print("Let me tell you a secret..........")
-            time.sleep(1)
-            print("YOU ARE ALWAYS RIGHT IN MAKING DECISIONS!!")
-            print("🙂🙂🙂🙂🙂🙂")   
-            time.sleep(5)
-            post_login()
-
-def insert(usr):
-    # this function inserts data into the myp_data table along with the help
-    # of inintodata() function 
-    web = input("Web address:  ")
-    logName = input(f"Enter username in {web}: ")
-    logPass = encrypt(pwinput(f"Enter the password for {logName}: "))
-        
-    #masterpass checking
-    uId = str(usr)
-    mycur.execute("USE myp;")
-    action = f"""SELECT masterPass FROM myp_users WHERE userId = {uId}"""
-    mycur.execute(action)
-                
-    for i in mycur:
-        u = str(i[0])
-
-    hello = False
-    while hello is False:
-        mp = str(pwinput("Enter your Master Password: "))           
-        hash = md5(mp.encode())
-        hashd = hash.hexdigest()
-        hash_crypt = encrypt(hashd)
-        mp_verify = hash_crypt
-
-        if mp_verify == u:
-            hello = True
-            insintodata(web, logName, logPass, uId)
-            
-        else:
-            print("PLEASE DOUBLE CHECK YOUR PASSWORD AS THERE IS NO USER IN OUR DATABASE WITH THIS PASSWORD")
-            hello = False
+    pass
 
 def otpmail(receivermail):
     # sends an otp to the receivermail  
@@ -280,58 +133,6 @@ def byemail(receivermail):
         server.send_message(msg)
         server.quit()
 
-def login():
-    #displays a list of the users singed up
-    global user
-    os.system('clear')
-    mycur.execute("USE myp;")
-    mycur.execute("SET @ROW := 0 ;")
-    mycur.execute(f"""SELECT @ROW := @ROW + 1 , userName FROM myp_users;""") 
-    data = mycur.fetchall()
-
-    mycur.execute(f"""SELECT userName FROM myp_users""") 
-    data_ls = mycur.fetchall()
-    
-    ls = []
-    for i in data_ls:
-        ls.extend(i)   
-    datadict = {}
-    for j in range(len(ls)):
-        datadict[j+1] = ls[j]
-
-    # ask the user to select which user wants to sign in
-    if datadict != {}:
-        print(tabulate(data, headers=['Sl.No.', 'Username'], tablefmt='psql'))
-        print("Enter the number corresponding to the user name to login: ")   
-        datain = int(input())
-        uname_srch = datadict.get(datain)
-        mycur.execute(f"SELECT userId FROM myp_users WHERE userName = '{uname_srch}';")
-        user_ls = []
-        for i in mycur:
-            user_ls.extend(i)
-        
-        user = user_ls[0]
-        mycur.execute(f"SELECT masterPass FROM myp_users WHERE userName = '{uname_srch}';")
-        
-        pass_ls = []
-        for i in mycur:
-            pass_ls.extend(i)
-        
-        #asks the masterpass for the selected user
-        ch = False
-        while ch == False:
-            masterPass_check_hash = hashcrypt(pwinput("Password: "))
-            if masterPass_check_hash == pass_ls[0]: 
-                ch = True 
-                post_login()
-            else:
-                print("Access Denied")
-        pass_ls = []
-    else:
-        print("There are no users logged in.")
-        time.sleep(3)
-        login_page()
-
 def signup():
     #asks users their details and a masterpass for signing up
     w = 't'
@@ -389,7 +190,6 @@ def signup():
             otp_count += 1
             otp_check = int(input("Please double check your OTP: "))
             if otp_count > 1:
-                os.system('clear')
                 print('Resend OTP?')
                 c = int(input("1.Yes\n2.No\n3.Exit"))
                 if c == 1:
@@ -425,93 +225,4 @@ def signup():
             print("YOUR PASSWORDS DON'T MATCH!")
 
     time.sleep(9)
-    os.system('clear')
-    login()
-
-def login_page():
-    # this is the first page when u run the program
-    # it asks the user whether they are signing in or creating their account
-    os.system('clear')
-
-    print('''-----------MENU-----------
-    1. Login
-    2. Sign Up
-    3. Exit''')
-
-    opt_lp = int(input(""))
-    
-    if opt_lp == 1:
-        login()
-        
-    elif opt_lp == 2:
-        signup()        
-
-    elif opt_lp == 3:
-        os.system('clear')
-        sys.exit            
-
-def post_login():
-
-    # as the functions name defines this is the second page post login
-    os.system('clear')
-
-    print('''-----------MENU-----------
-    1. Retrieve Password\t2. Store Password
-    3. Generate Password\t4. Remove Passwords
-    5. Log Out\t\t\t6. Delete User
-    7. Exit''')
-
-    opt = int(input())
-    if opt == 1:
-        os.system('clear')
-        retrieve(user)
-
-    elif opt == 2:
-        insert(user)
-
-        time.sleep(2)
-        post_login()
-
-    elif opt == 3:
-        os.system('clear')
-        print("""-----------MENU-----------
-    1. Only generate password?
-    2. Or save it too?
-    3. Go Back
-    4. Exit""")
-
-        opt_lp = int(input())
-        if opt_lp == 1:
-            generate_pass()
-
-            time.sleep(7)
-            post_login()
-            
-        elif opt_lp == 2:
-            insert(user)
-
-            time.sleep(7)
-            post_login()
-
-        elif opt_lp == 3:
-            post_login()
-
-        elif opt_lp == 4:
-            sys.exit
-    
-    elif opt == 4:
-        deletedata(user)
-
-        time.sleep(7)
-        post_login()
-    
-    elif opt == 5:
-        os.system('clear')
-        login_page()
-
-    elif opt == 7:
-        os.system('clear')   
-        sys.exit
-    
-    elif opt == 6:
-        deleteuser(user)
+    #login()
--- main.py
+++ main.py
@@ -1,30 +1,177 @@
 from database import *          #####IMPORTING OUR COUSTOM MODULES
-from mypfuncs import *          
-#ProjectbyVarunAdhityaGB
+from mypfuncs import *     
+from tkinter import *   
+from PIL import ImageTk, Image
+from tkinter import messagebox  
+import tkinter.ttk as ttk
 ########################## CHECKING AND CREATING DATABASE #############################
 mycur.execute('SHOW SCHEMAS;')
 db_ls = []
-#ProjectbyVarunAdhityaGB
+
 for i in mycur:
     db_ls.extend(i)
 #ProjectbyVarunAdhityaGB    
-if 'MYP' in db_ls:
-    mycur.execute("USE MYP;")
+if 'myp' in db_ls:
+    mycur.execute("USE myp;")
 elif 'myp' in db_ls:
     mycur.execute("USE myp;")
 else:
     createDB()
     mycur.execute("USE myp;")
-#ProjectbyVarunAdhityaGB
+
 ########################## CHECKING AND CREATING TABLES ###############################
 mycur.execute('SHOW TABLES;')
 tb_ls = []
-#ProjectbyVarunAdhityaGB 
+ 
 for i in mycur:
     tb_ls.extend(i)
 if tb_ls == []:
     createTbls()
 else:
     pass
-#ProjectbyVarunAdhityaGB
-login_page()
+
+########################## FUNCTOINS ###############################
+
+
+def login_page():
+    root.destroy()
+    global lpg
+    lpg = Tk()
+    lpg.config(bg="#26242f")
+    lpg.title("Login")
+    lpg.geometry("400x300")
+    lpg.resizable(0,0)
+    lpg.iconbitmap(r"images/icon.ico")
+
+    clicked = StringVar()
+    passkey = StringVar()
+    cbvar = IntVar(value=0)
+    mycur.execute("SELECT userName FROM myp_users") 
+    data_ls = mycur.fetchall()
+    ls = ['Select a Username']
+
+    for i in data_ls:
+        ls.extend(i)
+        
+    def shbtn():
+        if (cbvar.get())== 1:
+            pass_ent.config(show='')
+        else:
+            pass_ent.config(show='*')
+
+    def pass_ui():
+        lpg.destroy()
+        global psl
+        psl = Tk()
+        psl.mainloop()
+
+
+    def login():
+        global lpg
+        mydb = sqlc.connect(host='localhost', user='root', passwd='root',)
+        mycur = mydb.cursor()
+
+        global user
+        mycur.execute("USE myp;")
+        
+        uname_srch = usern_dm.get()
+        print(uname_srch)
+        if uname_srch == 'Select a Username':
+            messagebox.showinfo("Info","No users found!!")
+        else:
+            mycur.execute(f"SELECT userId FROM myp_users WHERE userName = '{uname_srch}';")   
+            user_ls = []
+            for i in mycur:
+                    user_ls.extend(i)
+
+            try:
+                user = user_ls[0]
+            except IndexError:
+                war = Label(root, text="Select a user", font=("",10)).grid(row=2, column=2)
+            mycur.execute(f"SELECT masterPass FROM myp_users WHERE userName = '{uname_srch}';")
+            pass_ls = []
+            for i in mycur:
+                pass_ls.extend(i)
+
+            ch = False
+            while ch == False:
+                masterPass_check_hash = hashcrypt(passkey.get())
+                if masterPass_check_hash == pass_ls[0]:
+                    ch == True
+                    pass_ui()
+                else:
+                    msgbox = messagebox.showwarning("ERROR","WRONG PASSWORD!!")
+                    if msgbox == 'ok':
+                        break
+            pass_ls = []
+
+    def bk_rootw():
+        global lpg
+        lpg.destroy()
+        rootw()
+
+    bk_arrow = ImageTk.PhotoImage(Image.open("images/backarrow.png"))
+    ################# STYLES ##########################################
+    cb_style = ttk.Style()
+    cb_style.configure('R.TCheckbutton', foreground='white', background='#26242f')
+    
+    
+    ###WIDGETS
+    usern_lbl = Label(lpg, text="Username:", font=('', 14), bg='#26242f', fg="white")
+    pass_lbl = Label(lpg, text="Password:", font=('', 14), bg='#26242f', fg="white")
+    pass_ent = Entry(lpg, textvariable=passkey, show="*", font=('', 14), bg='#26242f', fg="white")
+    in_btn = Button(lpg, text="Sign In",command=login, font=('', 14), width=20, bg='#26242f', fg="white")
+    showpass_cb = ttk.Checkbutton(lpg, text="Show Password", variable=cbvar, onvalue=1, offvalue=0, command=shbtn, style='R.TCheckbutton' )
+    bk_btn = Button(lpg, image=bk_arrow, command=bk_rootw, activebackground='#26242f', borderwidth=0, bg='#26242f')
+
+    #SHOWING THEM
+    usern_lbl.place(relx=0.2, rely=0.3, anchor=CENTER)
+    pass_lbl.place(relx=0.2, rely=0.45, anchor=CENTER)
+    pass_ent.place(relx=0.7, rely=0.45, anchor=CENTER)
+    showpass_cb.place(relx=0.85, rely=0.55, anchor=CENTER)
+    in_btn.place(relx=0.5, rely=0.7, anchor=CENTER)
+    bk_btn.place(relx=0.1, rely=0.1, anchor=CENTER)
+
+    if ls == []:
+        response = messagebox.showinfo("Info","No users found!!")
+        Label(lpg, text=response).pack()
+    else:
+        usern_dm = ttk.Combobox(lpg, value=ls, font=('',14))
+        usern_dm.current(0)
+        usern_dm.place(relx=0.7, rely=0.3, anchor=CENTER)
+
+    lpg.mainloop()
+    
+def signup_page():
+    root.destroy()
+    global spg
+    spg = Tk()
+    spg.title("Sign Up")
+    spg.config(bg='#26242f')
+    spg.geometry("600x400")
+    spg.resizable(0,0)
+    spg.iconbitmap(r"images/icon.ico")
+    spg.mainloop()
+
+########################## TKINTER MAIN ###############################
+
+def rootw():
+    global root
+    root = Tk()
+    root.config(bg="#26242f")
+    root.title("Password Manager")
+    root.iconbitmap(r"images/icon.ico")
+    root.geometry("300x300")
+    root.resizable(0,0)
+
+    signin_btn = Button(root, text = "Sign In", command=login_page, bg='#26242f', fg='white', width=10, font=('',14))
+    signup_btn = Button(root, text = "Sign Up", command=signup_page, bg='#26242f', fg='white', width=10, font=('',14))
+    exit_btn = Button(root, text = "Exit", command=root.destroy, bg='#26242f', fg='white',width=10, font=('',14))
+
+    signin_btn.place(relx=0.5, rely=0.35, anchor=CENTER)
+    signup_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+    exit_btn.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+    root.mainloop()
+
+rootw()
